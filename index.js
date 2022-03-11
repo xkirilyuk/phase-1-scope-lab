87dbf5d87da5df7ea1c29f6f50cd6dc13eae3bc3
Xenia <kakirilyuk@gmail.com>
2022-03-10 20:45:59 -0500

    commit tree c254b0e492fedf703676069bef9552527114b57b

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+var customerName = 'bob';
+customerName;
+console.log(customerName);
+
+
+function upperCaseCustomerName() {
+   customerName = customerName.toUpperCase();
+    console.log(customerName);
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'value'
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = leastFavoriteCustomer.toUpperCase;
+}
